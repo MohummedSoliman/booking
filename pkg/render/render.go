4c6cf531da08eae052d86cf6cd882207d791b5ad
Mohamed Ibranim <mosuliman92@gmail.com>
2025-08-23 12:54:48 +0300

    add render html file with cache logic

---
+++ pkg/render/render.go
@@ -0,0 +1,74 @@
+// Package render is created for rendering the html pages and caches it.
+package render
+
+import (
+	"bytes"
+	"log"
+	"net/http"
+	"path/filepath"
+	"text/template"
+
+	"github.com/MohummedSoliman/booking/pkg/config"
+)
+
+var app *config.AppConfig
+
+func NewTemplate(a *config.AppConfig) {
+	app = a
+}
+
+func RenderTemplate(w http.ResponseWriter, temName string) {
+	var tc map[string]*template.Template
+	if app.UseCache {
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
+
+	t, ok := tc[temName]
+	if !ok {
+		log.Fatal("Could not get tempate from templates cache")
+	}
+
+	buf := new(bytes.Buffer)
+	err := t.Execute(buf, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.*")
+	if err != nil {
+		log.Println(err)
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		t, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+		matches, err := filepath.Glob("./templates/*.layout.*")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			t, err = t.ParseGlob("./templates/*.layout.*")
+			if err != nil {
+				return myCache, err
+			}
+		}
+		myCache[name] = t
+	}
+	return myCache, nil
+}
