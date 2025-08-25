873b50697aa8969092fa7173d5b669da43336a6d
Mohamed Ibranim <mosuliman92@gmail.com>
2025-08-25 06:44:38 +0300

    add model struct

---
+++ pkg/models/templatedata.go
@@ -0,0 +1,14 @@
+// Package models is struct that contains data send between templates.
+package models
+
+// TemplateData holds data sent from handlers to templates
+type TemplateData struct {
+	StringMap map[string]string
+	IntMap    map[string]int
+	FloatMap  map[string]float32
+	Data      map[string]any
+	CSRFToken string
+	Flash     string
+	Warning   string
+	Error     string
+}
