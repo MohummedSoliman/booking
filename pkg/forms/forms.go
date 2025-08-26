c131ac261fc79ba0d48614c1cc370933d98f6431
Mohamed Ibranim <mosuliman92@gmail.com>
2025-08-26 18:45:57 +0300

    add method for check for valid or not

--- pkg/forms/forms.go
+++ pkg/forms/forms.go
@@ -9,7 +9,7 @@ import (
 // Form create custom form struct
 type Form struct {
 	url.Values
-	errors errors
+	Errors errors
 }
 
 func New(data url.Values) *Form {
@@ -19,9 +19,16 @@ func New(data url.Values) *Form {
 	}
 }
 
+func (f *Form) Valid() bool {
+	return len(f.Errors) == 0
+}
+
 // Has checks if form field is in post and not empty
 func (f *Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
-
-	return x != ""
+	if x == "" {
+		f.Errors.Add(field, "This Field can not be blank")
+		return false
+	}
+	return true
 }
