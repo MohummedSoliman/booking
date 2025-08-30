d7754ebb0f5bd12ef9cd3f645437ccd46b339ba4
Mohamed Ibranim <mosuliman92@gmail.com>
2025-08-30 17:26:10 +0300

    create file for easily switch between dbs vendors

---
+++ repository/dbRepo/dbRepo.go
@@ -0,0 +1,21 @@
+// Package dbrepo for enable switching between db vendors more easily.
+package dbrepo
+
+import (
+	"database/sql"
+
+	"github.com/MohummedSoliman/booking/pkg/config"
+	"github.com/MohummedSoliman/booking/repository"
+)
+
+type postgresDBRepo struct {
+	App *config.AppConfig
+	DB  *sql.DB
+}
+
+func NewPostgresRepo(conn *sql.DB, a *config.AppConfig) repository.DatabaseRepo {
+	return &postgresDBRepo{
+		App: a,
+		DB:  conn,
+	}
+}
