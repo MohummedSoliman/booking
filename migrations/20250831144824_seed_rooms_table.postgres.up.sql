5d19e259671846d5d6c589a81236599f82be49b8
Mohamed Ibranim <mosuliman92@gmail.com>
2025-08-31 17:55:03 +0300

    add two seed one for adding rooms and another for adding restrictions

---
+++ migrations/20250831144824_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.rooms (room_name, created_at, updated_at)
+VALUES
+('Generals Quarters', CURRENT_DATE, CURRENT_DATE),
+('Majors Suite', CURRENT_DATE, CURRENT_DATE);
---
+++ migrations/20250831145200_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.restrictions (restriction_name, created_at, updated_at)
+VALUES
+('Reservation', CURRENT_DATE, CURRENT_DATE),
+('Owner Block', CURRENT_DATE, CURRENT_DATE);
