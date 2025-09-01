939515273d4a67b8bbe13d38fa4e9d4c92dee6c5
Mohamed Ibranim <mosuliman92@gmail.com>
2025-09-01 14:24:17 +0300

    add test file for testing db methods

--- repository/dbRepo/test_repo.go
+++ repository/dbRepo/test_repo.go
@@ -1,6 +1,7 @@
 package dbrepo
 
 import (
+	"errors"
 	"time"
 
 	"github.com/MohummedSoliman/booking/pkg/models"
@@ -31,6 +32,9 @@ func (m *testDBRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]mode
 
 func (m *testDBRepo) GetRoomByID(id int) (models.Room, error) {
 	var room models.Room
+	if id > 2 {
+		return room, errors.New("some error")
+	}
 
 	return room, nil
 }
