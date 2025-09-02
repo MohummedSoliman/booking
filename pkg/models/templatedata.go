e8ffc52971c466e7e58cc74850d00ffe0f50d4eb
Mohamed Ibranim <mosuliman92@gmail.com>
2025-09-02 23:08:14 +0300

    add boolean flag for saving user logged in or not

--- pkg/models/templatedata.go
+++ pkg/models/templatedata.go
@@ -5,13 +5,14 @@ import "github.com/MohummedSoliman/booking/pkg/forms"
 
 // TemplateData holds data sent from handlers to templates
 type TemplateData struct {
-	StringMap map[string]string
-	IntMap    map[string]int
-	FloatMap  map[string]float32
-	Data      map[string]any
-	CSRFToken string
-	Flash     string
-	Warning   string
-	Error     string
-	Form      *forms.Form
+	StringMap       map[string]string
+	IntMap          map[string]int
+	FloatMap        map[string]float32
+	Data            map[string]any
+	CSRFToken       string
+	Flash           string
+	Warning         string
+	Error           string
+	Form            *forms.Form
+	IsAuthenticated bool
 }
