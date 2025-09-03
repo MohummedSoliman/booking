0e067c2502497625aa1be595ddceeda3657dde70
Mohamed Ibranim <mosuliman92@gmail.com>
2025-09-03 09:50:31 +0300

    add new routes for new admin dashboard links

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -35,6 +35,14 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Post("/user", handlers.Repo.PostLogin)
 	mux.Get("/logout", handlers.Repo.Logout)
 
+	mux.Route("/admin", func(mux chi.Router) {
+		mux.Use(Auth)
+		mux.Get("/dashboard", handlers.Repo.AdminDashboard)
+		mux.Get("/reservations-new", handlers.Repo.AdminNewReservations)
+		mux.Get("/reservations-all", handlers.Repo.AdminAllReservations)
+		mux.Get("/reservations-calendar", handlers.Repo.AdminReservationsCalendar)
+	})
+
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
